204249584e7518e0e6c4dd2e24edac3269d03c82
audionekt <audionekt@gmail.com>
2021-11-07 18:16:16 -0600

    forgot to add important files ;) haha

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+set args $argv
+echo running $argv.sql, hang tight..
+psql --host=127.0.0.1 --port=5432 --dbname=$DB_NAME --username=$DB_USER -a -f ../scripts/sql/$argv.sql
+exit
---
+++ scripts/sql/clear_db.sql
@@ -0,0 +1,5 @@
+DELETE FROM artist;
+DELETE FROM band;
+DELETE FROM artist_followers;
+DELETE FROM band_members;
+DELETE FROM band_fans;
